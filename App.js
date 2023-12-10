4f47f38a546a7a52c5a33d092c02f041a34af599
Alex Chukrii <chukrii.alex@gmail.com>
2023-12-10 20:53:11 +0100

    Configued splash screen in Expo

--- App.js
+++ App.js
@@ -1,41 +1,66 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { useFonts } from "expo-font";
+import * as Font from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
+// Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "opensans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-    "opensans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          "opensans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+          "opensans-regular": require("./assets/fonts/OpenSans-Regular.ttf"),
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
       await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [appIsReady]);
 
-  if (!fontsLoaded) {
+  if (!appIsReady) {
     return null;
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontFamily: "opensans-bold", fontSize: 30 }}>
-        Inter Black
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onLayout={onLayoutRootView}
+    >
+      <Text
+        style={[
+          {
+            fontFamily: "opensans-regular",
+          },
+        ]}
+      >
+        SplashScreen Demo! 👋
       </Text>
-      <Text style={{ fontSize: 30 }}>Platform Default</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
